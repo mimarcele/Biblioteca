3c0eb5af018b442712740a58e776e02f380745ef
mimarcele <milenamarcele777@gmail.com>
2023-02-26 22:17:26 -0300

    populando banco de dados

--- resources/data.sql
+++ resources/data.sql
@@ -1,3 +1,3 @@
---INSERT INTO livrosDto VALUES (1,'Roberto', 'Romance', 70)
---INSERT INTO livrosDto VALUES (2,'Luciana', 'Drama', 200)
---INSERT INTO livrosDto VALUES (3,'Milena', 'Terror', 10)
+INSERT INTO livrosDto VALUES (1,'Roberto', 'Romance', 70)
+INSERT INTO livrosDto VALUES (2,'Luciana', 'Drama', 200)
+INSERT INTO livrosDto VALUES (3,'Milena', 'Terror', 10)
